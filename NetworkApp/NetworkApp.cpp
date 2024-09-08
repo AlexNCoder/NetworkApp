aa7183848ba3ddeb5cf0cc7d437da00017aac881
unknown <alex.neurocoder@gmail.com>
2024-09-08 05:15:37 +0300

    Initial commit

---
+++ NetworkApp/NetworkApp.cpp
@@ -0,0 +1,20 @@
+﻿// NetworkApp.cpp: определяет точку входа для приложения.
+//
+
+#include "NetworkApp.h"
+//#include <cpp/mosquittopp.h>
+#include <mosquitto.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake1." << endl;
+	const char* id = new char(1);
+	void* obj = nullptr;
+	//mosqpp::mosquittopp();
+	auto res = mosquitto_new(id, false, obj);
+	cout << "Hello moscuitto" << endl;
+
+	return 0;
+}
