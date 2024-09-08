d5d5b2167d63dce1b987703ce8c07e9693cc2a2c
AlexNCoder <alex.neurocoder@gmail.com>
2024-09-09 01:23:59 +0300

    Успешная тестовая отправка и получение сообщения

--- NetworkApp/NetworkApp.cpp
+++ NetworkApp/NetworkApp.cpp
@@ -13,8 +13,21 @@ int main()
 	const char* id = new char(1);
 	void* obj = nullptr;
 	//mosqpp::mosquittopp();
-	auto res = mosquitto_new(id, false, obj);
+	mosquitto_lib_init();
+	auto mosq = mosquitto_new(NULL, true, obj);
+	std::string hostS = "localhost";
+	char* host = const_cast<char*>(hostS.c_str());
+	auto connRes = mosquitto_connect(mosq, host, 1883, 0);
+	
+	int* mid = new int;
+	const char* topic = "testTopicANC\0";
+	int payloadlen = 7;
+	const void* payload = "testMsg";
+	int qos = 0;
+	bool retain = true;
+	auto res = mosquitto_publish(mosq, mid, topic, payloadlen, payload, qos, retain);
 	cout << "Hello moscuitto" << endl;
+	mosquitto_destroy(mosq);
 
 	return 0;
 }
