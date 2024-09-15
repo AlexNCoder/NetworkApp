6805b8ff5a95af55500ba14d93fd6b2c8e6e8bce
AlexNCoder <alex.neurocoder@gmail.com>
2024-09-15 22:00:33 +0300

    (DRAFT) Получение клиентом mosquitto_sub тестового сообщения с отсортирован.числами и их суммой

--- NetworkAppPseudoServer/src/networkapppseudoserver.cpp
+++ NetworkAppPseudoServer/src/networkapppseudoserver.cpp
@@ -50,30 +50,23 @@ public:
 void Server::server_message_callback(mosquitto* mosq, void* userdata, const mosquitto_message* message)
 {
 	auto msgText = (char*)message->payload;
+	
 	std::cout << "From Server:	" << msgText << std::endl;
 	auto topic = Parser::makeOutTopic(message->topic);
-	std::string s = "8 jhkfd8 1,2 e6.3fa5o   75e1";
-	//message = "17 water, 26.33 shugar, 13,76 milk and 1 egg";
-	auto nums = Parser::parseMessage(s);
-	std::sort(nums.begin(), nums.end());
-	std::cout << topic << std::endl;
-	
-	/// ...обработка
-	auto serveranswer = "serveranswer";
-	/// ...обработка
-	
-	pub(mosq, serveranswer, topic);
+
+	//std::string s = "17 water, 26.33 shugar, 13,76 milk and 1 egg";
+	//std::string s = "8 jhkfd8 1,2 e6.3fa5o   75e1";
+	auto nums = Parser::parseMessage(msgText);
+	//return;
+	auto send = sumStr(nums);
+	pub(mosq, send.c_str(), topic);
 }
 
-void Server::run()
+std::string Server::sumStr(std::vector<std::string>& nums)
 {
-	//std::string s = "8 jhkfd8 1,2 e6.3fa5o   75e1";
-	std::string s = "17 water, 26.33 shugar, 13,76 milk and 1 egg";
-	auto nums = Parser::parseMessage(s);
-	auto numsCopy = nums;
-	std::sort(nums.begin(), nums.end());
 	std::vector<double> numsF;
 	auto sum = 0.0;
+
 	for (auto e : nums)
 	{
 		auto f = atof(e.c_str());
@@ -81,9 +74,30 @@ void Server::run()
 
 		sum += f;
 	}
+	std::sort(numsF.begin(), numsF.end());
+
+	std::string res;
+
+	for (auto e : numsF)
+	{
+		res.append(std::to_string(e).c_str());
+		res.append(" ");
+	}
+	res.erase(res.find_last_of(" "));
+	
+	res.append("\n");
+	res.append(std::to_string(sum).c_str());
+	std::cout << res << std::endl;
+	//res.push_back(itoa(0,));
+
+	return res;
+}
+
+void Server::run()
+{
 	sub(server_message_callback, "/testTopicANC/in/");
-	//sub();
-	//while (1)
+	
+	while (1)
 	{
 		mosquitto_loop(m_mosq, 100, 1);
 	}
--- NetworkAppPseudoServer/src/networkapppseudoserver.h
+++ NetworkAppPseudoServer/src/networkapppseudoserver.h
@@ -1,10 +1,13 @@
 ﻿#pragma once
 #include <mosquittoconnect.h>
+#include <string>
+#include <vector>
 
 class Server : public MosquittoConnect
 {
 public:
 	static void server_message_callback(struct mosquitto* mosq, void* userdata, const struct mosquitto_message* message);
+	static std::string sumStr(std::vector<std::string>& nums);
 	void run() override;
 
 private:
