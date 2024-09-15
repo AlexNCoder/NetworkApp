4383d9bae88e6eea5c58adf0bf49b24b46b51821
AlexNCoder <alex.neurocoder@gmail.com>
2024-09-15 17:27:45 +0300

    (DRAFT) Тестовый ответ от сервера полностью готов

--- NetworkAppPseudoServer/src/networkapppseudoserver.cpp
+++ NetworkAppPseudoServer/src/networkapppseudoserver.cpp
@@ -2,20 +2,22 @@
 #include <iostream>
 #include "mosquittoconnect.h"
 #include <cstring>
+#include <regex>
+#include <sstream>
 
 class Parser
 {
 public:
-	static std::string f(std::string str)
+	static std::string makeOutTopic(std::string currentTopic)
 	{
-		auto pos = str.rfind("/in/");
-		if (pos == str.npos)
+		auto pos = currentTopic.rfind("/in/");
+		if (pos == currentTopic.npos)
 		{
 			return "";
 		}
-		str.replace(pos, str.size() - 1, "/out/");
+		currentTopic.replace(pos, currentTopic.size() - 1, "/out/");
 
-		return str;
+		return currentTopic;
 		
 		
 			/*
@@ -27,12 +29,33 @@ public:
 			pch = strtok(NULL, "/");
 		}*/
 	}
+
+	static std::vector<std::string> parseMessage(std::string message)
+	{
+		std::regex ex(R"(((\d+)[,.](\d+))|(\d+))");
+		
+		message = std::regex_replace(message, std::regex("[^0-9,.]"), " ");
+		message = std::regex_replace(message, std::regex(","), ".");
+
+		std::vector<std::string> res;
+		std::copy(
+			std::sregex_token_iterator(message.begin(), message.end(), ex, 0),
+			std::sregex_token_iterator(),
+			std::back_inserter(res));
+
+		return res;
+	}
 };
 
 void Server::server_message_callback(mosquitto* mosq, void* userdata, const mosquitto_message* message)
 {
-	std::cout << "From Server:	" << (char*)message->payload << std::endl;
-	auto topic = Parser::f(message->topic);
+	auto msgText = (char*)message->payload;
+	std::cout << "From Server:	" << msgText << std::endl;
+	auto topic = Parser::makeOutTopic(message->topic);
+	std::string s = "8 jhkfd8 1,2 e6.3fa5o   75e1";
+	//message = "17 water, 26.33 shugar, 13,76 milk and 1 egg";
+	auto nums = Parser::parseMessage(s);
+	std::sort(nums.begin(), nums.end());
 	std::cout << topic << std::endl;
 	
 	/// ...обработка
@@ -44,9 +67,23 @@ void Server::server_message_callback(mosquitto* mosq, void* userdata, const mosq
 
 void Server::run()
 {
+	//std::string s = "8 jhkfd8 1,2 e6.3fa5o   75e1";
+	std::string s = "17 water, 26.33 shugar, 13,76 milk and 1 egg";
+	auto nums = Parser::parseMessage(s);
+	auto numsCopy = nums;
+	std::sort(nums.begin(), nums.end());
+	std::vector<double> numsF;
+	auto sum = 0.0;
+	for (auto e : nums)
+	{
+		auto f = atof(e.c_str());
+		numsF.push_back(f);
+
+		sum += f;
+	}
 	sub(server_message_callback, "/testTopicANC/in/");
 	//sub();
-	while (1)
+	//while (1)
 	{
 		mosquitto_loop(m_mosq, 100, 1);
 	}
