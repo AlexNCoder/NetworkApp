03e07b97e0dac933cc08a35ff2448d3e82ba9c46
AlexNCoder <alex.neurocoder@gmail.com>
2024-09-15 22:09:29 +0300

    (DRAFT) Получение сообщения с консоли клиента и отправка на сервер

--- NetworkAppClient/main.cpp
+++ NetworkAppClient/main.cpp
@@ -1,6 +1,5 @@
+#include <iostream>
 #include "src/NetworkAppClient.h"
-
-
 #include "src/parser.h"
 
 int main()
@@ -8,10 +7,8 @@ int main()
 	// Парсинг командной строки (неограниченная длина, только ASCII)
 	// Пример: 
 	//	17 water, 26.33 shugar, 13,76 milk and 1 egg
-	std::string s = "13.76 26.33 1 17";
+	Client().run();
 
-	// Отправка сообщения
-	
 	// Получение ответа от сервера
 
 	// Отображение результата
--- NetworkAppClient/src/networkappclient.cpp
+++ NetworkAppClient/src/networkappclient.cpp
@@ -1,6 +1,7 @@
 ﻿#include "networkappclient.h"
 #include <iostream>
 #include <mosquitto.h>
+#include <string>
 
 void Client::message_callback(mosquitto* mosq, void* userdata, const mosquitto_message* message)
 {
@@ -9,4 +10,14 @@ void Client::message_callback(mosquitto* mosq, void* userdata, const mosquitto_m
 
 void Client::run()
 {
+	//std::string s = "13.76 26.33 1 17";
+
+	std::string s;
+
+	// Отправка сообщения
+	while (1)
+	{
+		std::getline(std::cin, s);
+		pub(m_mosq, s.c_str(), "/testTopicANC/in/");
+	}
 }
